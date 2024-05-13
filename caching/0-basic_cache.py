a55167a351c12c80aa79289484c021200491d67b
danielburongu <d.burongu@alustudent.com>
2024-05-13 20:26:44 +0300

    caching

---
+++ caching/0-basic_cache.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+BasicCache class that inherits from BaseCaching and is a caching system
+"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """ Define BasicCache """
+
+    def put(self, key, item):
+        """ Add key/value pair to the cache """
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """ Retrieve value from the cache based on given key """
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data.get(key)
