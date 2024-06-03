2787f90aaf616e539bb7dcdf974f05c19c5aad55
danielburongu <d.burongu@alustudent.com>
2024-06-03 12:11:43 +0200

    E302 fixed on user model

--- user_authentication_service/user.py
+++ user_authentication_service/user.py
@@ -7,7 +7,7 @@ Base = declarative_base()
 
 class User(Base):
     __tablename__ = 'users'
-    
+
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
@@ -16,8 +16,12 @@ class User(Base):
 
     def hash_password(self, password):
         """Hashes a password using bcrypt."""
-        self.hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+        self.hashed_password = bcrypt.hashpw(
+            password.encode('utf-8'), bcrypt.gensalt()
+        ).decode('utf-8')
 
     def verify_password(self, password):
         """Verifies the password against the stored hashed password."""
-        return bcrypt.checkpw(password.encode('utf-8'), self.hashed_password.encode('utf-8'))
+        return bcrypt.checkpw(
+            password.encode('utf-8'), self.hashed_password.encode('utf-8')
+        )
