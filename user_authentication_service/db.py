7a0d77eb5020d6a61c67d5c7e873d62fff5816f9
danielburongu <d.burongu@alustudent.com>
2024-06-03 13:10:54 +0200

    create user and add user DB

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -4,6 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from user import Base, User
 
+
 class DB:
     def __init__(self):
         self._engine = create_engine("sqlite:///a.db", echo=True)
